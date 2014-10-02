33ed2612bd439a5a5cdefefe62d0e4393cf8933d
mpkostek <mpkostek@buffalo.edu>
2014-10-02 12:54:58 -0400

    Todo app

---
+++ TodoApp2/AddViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddViewController.swift
+//  TodoApp2
+//
+//  Created by Mike Kostek on 10/1/14.
+//  Copyright (c) 2014 UB. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var notesTextView: UITextView!
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButtonTapped(sender: AnyObject) {
+        
+        var dataSet:NSMutableDictionary  = NSMutableDictionary()
+        dataSet.setObject(titleTextField.text, forKey: "itemTitle")
+        dataSet.setObject(notesTextView.text, forKey: "itemNote")
+        
+    }
+    
+
+}
