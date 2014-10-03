145596ca32bb48d9c3c25b5a448f960d6b6011d5
mpkostek <mpkostek@buffalo.edu>
2014-10-03 13:11:11 -0400

    TodoApp3

--- TodoApp3/TodoApp3/AppDelegate.swift
+++ TodoApp3/TodoApp3/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  TodoApp3
+//  TodoApp2
 //
-//  Created by Michael Kostek on 10/3/14.
-//  Copyright (c) 2014 Michael Kostek. All rights reserved.
+//  Created by Mike Kostek on 10/1/14.
+//  Copyright (c) 2014 UB. All rights reserved.
 //
 
 import UIKit
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
         return true
     }
 
@@ -35,8 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
+    }   
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
