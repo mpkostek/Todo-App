85dd21bbaba9a05ae4c279036dcbbc4da6de27d2
mpkostek <mpkostek@buffalo.edu>
2014-10-03 13:25:59 -0400

    Part 2 17:50

--- TodoApp3/TodoApp3/AddViewController.swift
+++ TodoApp3/TodoApp3/AddViewController.swift
@@ -28,6 +28,27 @@ class AddViewController: UIViewController {
     
     @IBAction func addButtonTapped(sender: AnyObject) {
         println("Button Tapped")
+        
+        var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        var itemList:NSMutableArray? = userDefaults.objectForKey("itemList") as? NSMutableArray //optional variable, app doesn't crash if there is no value for the key
+        
+        if (itemList){ //data already avaialable
+        
+            
+            
+            //TODO part2 video 17:45
+            
+            
+            
+        }
+        else { //this is the first todo item in the list
+            userDefaults.removeObjectForKey("itemList") //not necessary, used as a safety check
+            itemList = NSMutableArray()
+            itemList!.addObject(dataSet) //unwrap the array by using the !, unwrap it and add an object
+            userDefaults.setObject(itemList, forKey: "itemList")
+        }
+        
         var dataSet:NSMutableDictionary  = NSMutableDictionary()
         dataSet.setObject(titleTextField.text, forKey: "itemTitle")
         dataSet.setObject(notesTextView.text, forKey: "itemNote")
